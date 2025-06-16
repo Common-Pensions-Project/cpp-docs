ee2df1edd2491175ba0fa510918a61fe863bb808
xx213 <rob.mann@rpmitconsulting.com>
2025-06-16 21:54:02 +0100

    Docusaurus added

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/blog',
     component: ComponentCreator('/blog', 'b2f'),
     exact: true
@@ -79,19 +114,31 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '4a8'),
+    component: ComponentCreator('/docs', '52b'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', '9e2'),
+        component: ComponentCreator('/docs', '303'),
         routes: [
           {
             path: '/docs',
-            component: ComponentCreator('/docs', 'e1a'),
+            component: ComponentCreator('/docs', '52e'),
             routes: [
               {
                 path: '/docs/',
                 component: ComponentCreator('/docs/', '4a8'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/adr/ADR-0001-example',
+                component: ComponentCreator('/docs/adr/ADR-0001-example', '362'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/adr/ADR-0001-schema-governance',
+                component: ComponentCreator('/docs/adr/ADR-0001-schema-governance', '7c0'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
@@ -116,18 +163,6 @@ export default [
               {
                 path: '/docs/category/tutorial---extras',
                 component: ComponentCreator('/docs/category/tutorial---extras', '9ad'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/docs/decisions/ADR-0001-example',
-                component: ComponentCreator('/docs/decisions/ADR-0001-example', 'ea1'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/docs/decisions/ADR-0001-schema-governance',
-                component: ComponentCreator('/docs/decisions/ADR-0001-schema-governance', '479'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
